f131c3ce33c33672f3f39419097ad9f34597cf71
gaponukz <gaponukz54@gmail.com>
2022-05-27 14:06:15 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { CONTACT_ABI, CONTACT_ADDRESS } from './config'
+import Web3 from 'web3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [account, setAccount] = useState()
+	const [roomUsersCount, setRoomUsersCount] = useState()
+
+	const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
+	const game = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS)
+
+	useEffect(() => {
+		(async () => {
+			const accounts = await web3.eth.requestAccounts()
+			setRoomUsersCount(await game.methods.roomUsersCount().call())
+			setAccount(accounts[0])
+		})()
+	}, []);
+	
+	return (
+		<div>
+       		Your account is: {account}, rooms count: {roomUsersCount}
+
+			<button onClick={async () => {
+				game.methods.addUserToGame().send({
+					from: account,
+					gas: 3200000,
+					value: web3.utils.toWei("0.025", "ether")
+				}, (error, result) => {
+					if (error) {
+						alert("You are not in game")
+					}
+				})
+			}}>Add me to game</button>
+     	</div>
+	);
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,106 @@
+export const CONTACT_ADDRESS = '0xd7d1b65eAaa6C5098c80e6bACAf401f4932e608B';
+
+export const CONTACT_ABI = [
+    {
+      "inputs": [],
+      "name": "counter",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "roomAddresses",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "roomUsersCount",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "rooms",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "addUserToGame",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "limit",
+          "type": "uint256"
+        }
+      ],
+      "name": "random",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    }
+]
